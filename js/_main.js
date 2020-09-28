6ff384d61035cfbb1d4511806cdbf288f2e5f4f9
Federico <natalini.1710691@studenti.uniroma1.it>
2020-09-28 18:00:06 +0200

    fix bug and add opinion checkbox

--- js/_main.js
+++ js/_main.js
@@ -1,12 +1,18 @@
 
 $("#submitComment").click(function () {
     var comment = $("#addComment").val();
-    if(comment!=""){
+    if (comment != "") {
         $("#commentDone").append(
-        ' <p class="badge badge-light">Luca</p>'+'<div class="card"><div class="card-body">' + comment + '</div></div>');                      }
-    })
+            ' <p class="badge badge-light">Luca</p>' + '<div class="card"><div class="card-body">' + comment + '</div></div>');
+    }
+})
 
-    $(document).ready(function () {
-        var n = $(".card").length;
-        $("#count").html(n);
-    })
+$(document).ready(function () {
+    var n = $(".card").length;
+    $("#count").html(n);
+})
+
+$('.form-check-input').click(function(){
+    var opinion=$('.form-check-input:checked').val();
+    $('#opinion').html("Hai ritenuto l'articolo "+opinion+'. Grazie per il feedback!')
+})
